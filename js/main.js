45dd456bb1693fa2d4d2436bfed3d8bc5713adb7
alex <al3xback@gmail.com>
2024-08-31 15:52:06 +0700

    added first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const cardEl = document.querySelector('.card');
+
+/* Array at() */
+const amounts = [0.039, 0.040, 0.041];
+const ethereumAmount = amounts.at(2);
+
+const cardStatusItemEls = cardEl.querySelectorAll('.card__stats-list-item');
+cardStatusItemEls[0].querySelector(
+	'span'
+).textContent = `${ethereumAmount} ETH`;
+
+/* String at() */
+const dummyNumber = '123';
+const remainingTime = dummyNumber.at(2);
+
+cardStatusItemEls[1].querySelector(
+	'span'
+).textContent = `${remainingTime} days left`;
+
+/* Class field declarations */
+class Author {
+	name = 'Jules Wyvern';
+	image = 'avatar.png';
+}
+
+const author = new Author();
+
+const cardAuthorImageEl = cardEl.querySelector('.card__author-img');
+cardAuthorImageEl.src = './images/' + author.image;
+
+const cardAuthorNameEl = cardEl.querySelector('.card__author-desc a');
+cardAuthorNameEl.textContent = author.name;
+
+/* Private methods and fields */
+class ClonedAuthor extends Author {
+	#shortName = 'Jules';
+	#printName() {
+		console.log(this.name);
+	}
+}
+
+const clonedAuthor = new ClonedAuthor();
+console.log(clonedAuthor);
